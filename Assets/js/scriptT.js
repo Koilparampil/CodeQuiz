9eb7f88ddc50eb4c2053db793cf13d52593fcc10
KreshsDesktop <ckresh99@gmail.com>
2022-01-24 21:07:01 -0500

    Created Test Page HTML, working on javascript, hard coded in the Questions

---
+++ Assets/js/scriptT.js
@@ -0,0 +1,42 @@
+var question =document.querySelector(".question");
+var option1 =document.querySelector(".option1");
+var option2 =document.querySelector(".option2");
+var option3 =document.querySelector(".option3");
+var RightorWrong =document.querySelector("#CorIn");
+
+
+const Question1={
+    questionC="What are variables used for in JavaScript?",
+    option1C='For Changing a value’s data type.',
+    option2C='For Changing language settings.',
+    option3C='For storing or holding data.',
+    CorrectO=3
+}
+const Question2={
+    questionC="Which of the following is an example of a single line comment?",
+    option1C='‘Is this a comment?’',
+    option2C='// Is this a Comment?',
+    option3C='Console.log(‘Is this a comment?’);',
+    CorrectO=2
+}
+const Question3={
+    questionC="What is String Concatenation?",
+    option1C='When you assign a string to a variable. ',
+    option2C='When you change a variable’s Value.',
+    option3C='When you join strings together.',
+    CorrectO=3
+}
+const Question4={
+    questionC="What is the correct way to call the ‘random’ method on the Math global object?",
+    option1C='Math.random()',
+    option2C='math.random()',
+    option3C='Math(random)',
+    CorrectO=1
+}
+const Question5={
+    questionC="Which of the following statements are true?",
+    option1C='Objects store unordered data of any type as key-value pairs.',
+    option2C='Objects cant store other objects. ',
+    option3C='Objects only store strings ',
+    CorrectO=1
+}
